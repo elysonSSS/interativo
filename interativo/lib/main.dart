3b8727aae3a81ddf0de55d0b6227c6b9cb749878
elysonSSS <elysonsp4@gmail.com>
2024-08-20 21:36:53 -0300

    alterado todo layout

--- interativo/lib/main.dart
+++ interativo/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
+/// A simple Flutter app that displays a tourist attraction.
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'Flutter layout demo';
+    const String appTitle = 'Visite';
     return MaterialApp(
       title: appTitle,
       home: Scaffold(
@@ -18,26 +19,26 @@ class MyApp extends StatelessWidget {
         body: const SingleChildScrollView(
           child: Column(
             children: [
-              ImageSection(
-                image: 'images/lake.jpg',
-              ),
+              ImageSection(),
               TitleSection(
-                name: 'Oeschinen Lake Campground',
-                location: 'Kandersteg, Switzerland',
+                name: 'Catedral Nossa Senhora de Oliveira - Vacaria',
+                location: 'Vacaria, Brasil',
               ),
               ButtonSection(),
               TextSection(
                 description:
-                    'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
-                    'Bernese Alps. Situated 1,578 meters above sea level, it '
-                    'is one of the larger Alpine Lakes. A gondola ride from '
-                    'Kandersteg, followed by a half-hour walk through pastures '
-                    'and pine forest, leads you to the lake, which warms to 20 '
-                    'degrees Celsius in the summer. Activities enjoyed here '
-                    'include rowing, and riding the summer toboggan run.',
+                    'A Catedral Nossa Senhora da Oliveira, que se localiza na Praça Daltro Filho,'
+                    ' no centro da cidade. Projetada em 1912 por Jean-Louis Bernaz (Frei Efrem de Bellevaux),' 
+                    'a Catedral possui estilo gótico, com características semelhantes à Catedral de '
+                    'Notre Dame (Paris), e foi construída em pedra moura, material retirado do próprio '
+                    'município e finalizada em 1933. O local abriga a pequena imagem de madeira de Nossa '
+                    'Senhora da Imaculada Conceição da Oliveira, encontrada por um camponês em Vacaria por '
+                    'volta de 1750, fato de grande importância para o desenvolvimento da cidade. A Catedral '
+                    'foi tombada pelo município e está aberta diariamente, das 8h às 19h, com acesso na lateral '
+                    'livre aos visitantes.',
               ),
               TapboxA(),
-              MyCustomForm(),
+              NoteSection(),
             ],
           ),
         ),
@@ -46,6 +47,8 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+/// A section that displays the title and location of a tourist attraction.
 
 class TitleSection extends StatelessWidget {
   const TitleSection({
@@ -73,6 +76,7 @@ class TitleSection extends StatelessWidget {
                   padding: const EdgeInsets.only(bottom: 8),
                   child: Text(
                     name,
+
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
                     ),
@@ -96,6 +100,8 @@ class TitleSection extends StatelessWidget {
   }
 }
 
+/// A section that displays a row of buttons.
+
 class ButtonSection extends StatelessWidget {
   const ButtonSection({super.key});
 
@@ -109,23 +115,25 @@ class ButtonSection extends StatelessWidget {
           ButtonWithText(
             color: color,
             icon: Icons.call,
-            label: 'CALL',
+            label: 'Ligar',
           ),
           ButtonWithText(
             color: color,
             icon: Icons.near_me,
-            label: 'ROUTE',
+            label: 'Rota',
           ),
           ButtonWithText(
             color: color,
             icon: Icons.share,
-            label: 'SHARE',
+            label: 'Compartilhar',
           ),
         ],
       ),
     );
   }
 }
+
+/// A button with a text label.
 
 class ButtonWithText extends StatelessWidget {
   const ButtonWithText({
@@ -162,6 +170,8 @@ class ButtonWithText extends StatelessWidget {
   }
 }
 
+/// A section that displays a block of text.
+
 class TextSection extends StatelessWidget {
   const TextSection({
     super.key,
@@ -182,25 +192,55 @@ class TextSection extends StatelessWidget {
   }
 }
 
-// #docregion image-asset
-class ImageSection extends StatelessWidget {
-  const ImageSection({super.key, required this.image});
 
-  final String image;
+/// A section that displays a carroussel of images.
+
+class ImageSection extends StatefulWidget {
+  const ImageSection({super.key});
+
+  @override
+  State<ImageSection> createState() => _ImageSectionState();
+}
+
+class _ImageSectionState extends State<ImageSection> {
+  int _currentPage = 0;
+  final List<String> _images = [
+    'images/catedral.jpg',
+    'images/catedral2.jpg',
+    'images/catedral3.jpg',
+    'images/catedral4.jpg',
+    'images/catedral5.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // #docregion image-asset
-    return Image.asset(
-      image,
-      width: 600,
-      height: 240,
-      fit: BoxFit.cover,
+    return GestureDetector(
+      onHorizontalDragEnd: (details) {
+        if (details.velocity.pixelsPerSecond.dx > 0) {
+          // Deslizar para a direita
+          if (_currentPage < _images.length - 1) {
+            setState(() {
+              _currentPage++;
+            });
+          }
+        } else if (details.velocity.pixelsPerSecond.dx < 0) {
+          // Deslizar para a esquerda
+          if (_currentPage > 0) {
+            setState(() {
+              _currentPage--;
+            });
+          }
+        }
+      },
+      child: Image.asset(
+        _images[_currentPage],
+        width: 500,
+        height: 300,
+        fit: BoxFit.cover,
+      ),
     );
-    // #enddocregion image-asset
   }
 }
-// #enddocregion image-section
 
 // #docregion favorite-widget
 class FavoriteWidget extends StatefulWidget {
@@ -287,46 +327,57 @@ class _TapboxAState extends State<TapboxA> {
         height: 100,
         decoration: BoxDecoration(
           color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+          borderRadius: BorderRadius.circular(10),
         ),
-        child: Center(
-          child: Text(
-            _active ? 'Active' : 'Inactive',
-            style: const TextStyle(fontSize: 20, color: Colors.white),
-          ),
+        child: Icon(
+            _active ? Icons.check : Icons.bookmark_border,
+            size: 40, // Tamanho do ícone
+            color: Colors.white,
+
         ),
       ),
     );
   }
 }
 
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({super.key});
+class NoteSection extends StatefulWidget {
+  const NoteSection({super.key});
 
   @override
-  State<MyCustomForm> createState() => _MyCustomFormState();
+  NoteSectionState createState() => NoteSectionState();
 }
 
-// Define a corresponding State class.
-// This class holds the data related to the Form.
-class _MyCustomFormState extends State<MyCustomForm> {
-  // Create a text controller and use it to retrieve the current value
-  // of the TextField.
-  final myController = TextEditingController();
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    myController.dispose();
-    super.dispose();
-  }
+class NoteSectionState extends State<NoteSection> {
+  final _controller = TextEditingController();
+  String _note = '';
 
   @override
   Widget build(BuildContext context) {
-    // Fill this out in the next step.
-    return TextField(
-      controller: myController,
-);
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              labelText: 'Nota',
+              border: OutlineInputBorder(),
+            ),
+            maxLines: 5,
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _note = _controller.text;
+              });
+            },
+            child: const Text('Salvar'),
+          ),
+          const SizedBox(height: 16),
+          TextSection(description: _note),
+        ],
+      ),
+    );
   }
-
-
 }
